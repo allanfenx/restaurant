a0b6e1e14f42480f4c5987aee26acfa54658aaf7
allanfenx <allanfenx@hotmail.com>
2025-02-04 19:38:05 -0300

    add read.me

--- App.tsx
+++ App.tsx
@@ -3,11 +3,15 @@ import { Lobster_400Regular } from '@expo-google-fonts/lobster';
 import { Poppins_500Medium, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 import { Roboto_500Medium, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { Routes } from './src/routes';
-
+import { hideAsync } from "expo-splash-screen";
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({ Lobster_400Regular, Poppins_500Medium, Roboto_500Medium, Inter_500Medium, Roboto_700Bold, Roboto_400Regular, Poppins_600SemiBold })
+
+  if (!fontsLoaded) {
+    hideAsync()
+  }
 
 
   return (
---
+++ README.md
@@ -0,0 +1,18 @@
+# Foodgo
+
+## Está aplicação para restaurante. 
+
+## Ante de rodar essa aplicação em modo dev.
+
+#### Tenha certeza de ter instalado node js
+
+##### Para rodar basta executar comando abaixo.
+
+```
+npm start 
+
+or
+
+yarn start
+
+```
